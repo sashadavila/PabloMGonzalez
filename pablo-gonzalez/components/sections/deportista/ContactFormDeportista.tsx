bdd7d93b8a7871111bd4c539f03037464dbbe383
CarolinaAriza-mind <carolinaarizat93@gmail.com>
2026-04-14 20:38:02 -0300

    Finalizacion actualizaciones

--- pablo-gonzalez/components/sections/deportista/ContactFormDeportista.tsx
+++ pablo-gonzalez/components/sections/deportista/ContactFormDeportista.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export function ContactFormDeportistas() {
   const [loading, setLoading] = useState(false);
